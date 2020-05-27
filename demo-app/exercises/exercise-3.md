889521c54387db13a771e01137e26f8b89864190
Eric Greene <eric@t4d.io>
2020-05-27 17:59:40 -0400

    add code demos

---
+++ demo-app/exercises/exercise-3.md
@@ -0,0 +1,11 @@
+# Exercise 3
+
+1. Solve world hunger.
+
+2. Solve world peace.
+
+3. Create an array of cars objects. It will look similar to the array of color objects. Create the array within the Car Tool component.
+
+4. Using the array dynamically create the car rows.
+
+5. Ensure it works! Bon chance!
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 export const ColorTool = () => {
 
+  const colors = [
+    { id: 1, name: 'orange' },
+    { id: 2, name: 'green' },
+    { id: 3, name: 'blue' },
+    { id: 4, name: 'yellow' },
+  ];
+
+  // const colorListItems = [];
+
+  // for (let x=0; x<colors.length; x++) {
+  //   colorListItems.push(<li>{colors[x]}</li>);
+  // }
+
   return <>
     <header>
       <h1>Color Tool</h1>
     </header>
     <ul>
-      <li>orange</li>
-      <li>green</li>
-      <li>blue</li>
-      <li>yellow</li>
+      {colors.map( (c) => <li key={c.id}>{c.name}</li>)}
     </ul>
   </>;
 
