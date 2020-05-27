ab465956dc8bb15ab0043056ae2c4ba558b56a87
Eric Greene <eric@t4d.io>
2020-05-27 17:08:06 -0400

    add code demos

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -2,8 +2,42 @@ import React from 'react';
 
 export const CarTool = () => {
 
-  return <header>
-    <h1>Car Tool</h1>
-  </header>;
+  return (
+    <>
+      <header>
+        <h1>Car Tool</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+            <th>Color</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Ford</td>
+            <td>Fusion Hybrid</td>
+            <td>2019</td>
+            <td>white</td>
+            <td>45000</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Tesla</td>
+            <td>Model S</td>
+            <td>2020</td>
+            <td>red</td>
+            <td>130000</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 
 };
