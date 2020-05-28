20e90119c333eb8b4c64bfd1932528fa64380296
Eric Greene <eric@t4d.io>
2020-05-28 14:05:51 -0400

    add code demos

---
+++ demo-app/exercises/exercise-6.md
@@ -0,0 +1,9 @@
+# Exercise 6
+
+1. Add a form to the Car Tool component. Have fields for collecting make, model, year, color and price.
+
+2. As the user types into the fields, display the state of the color form in the console.
+
+3. Ensure it works.
+
+Hint: Please copy and paste code from Color Tool.
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ToolHeader } from './ToolHeader';
 import { ItemList } from './ItemList';
 
 export const ColorTool = (props) => {
 
+  // const colorFormStateArray = useState({
+  //   colorName: '',
+  //   colorHexcode: '',
+  // });
+
+  // // const colorFormStateData = colorFormStateArray[0];
+  // // const colorFormStateUpdateFn = colorFormStateArray[1];
+
+
+  // const [ colorFormStateData, colorFormStateUpdateFn ] = colorFormStateArray;
+
+  const [
+    colorForm, // state data
+    setColorForm, // update function - update the state, and trigger the re-render
+  ] = useState({
+    colorName: '',
+    colorHexcode: '',
+  } /* initial value of the state, it is only used on the first render */);
+
+  const change = (e) => {
+
+    setColorForm({
+      ...colorForm, // copy all of the properties into the new object
+      // code which will update the one properties (field) I am typing into
+      
+      // colorName: e.target.value,
+      
+      // computed property, the expression within the square braces is evaluation,
+      // and the result is the name of the property
+      // which will be updated
+      [ e.target.name ]: e.target.value,
+    });
+
+  };
+
+  console.log(colorForm);
+
   return <>
     <ToolHeader headerText="Color Tool" />
     <ItemList items={props.colors} />
+
+    <form>
+      <div>
+        <label htmlFor="color-name-input">Color Name:</label>
+        <input type="text" id="color-name-input" name="colorName" value={colorForm.colorName} onChange={change} />
+      </div>
+    </form>
+
   </>;
 
 };
