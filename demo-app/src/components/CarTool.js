000caa7675370df3b588b48967f2eab7d04b8006
Eric Greene <eric@t4d.io>
2020-05-28 16:55:22 -0400

    add solution exercise 8

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ToolHeader } from './ToolHeader';
 import { CarTable } from './CarTable';
+import { CarForm } from './CarForm';
 
 export const CarTool = (props) => {
+
+  const [ cars, setCars ] = useState(props.cars.concat());
+
+  const addCar = (car) => {
+    setCars(cars.concat({
+      ...car,
+      id: Math.max(...cars.map(c => c.id), 0) + 1,
+    }));
+  };
 
   return (
     <>
       <ToolHeader headerText="Car Tool" />
-      <CarTable cars={props.cars} />
+      <CarTable cars={cars} />
+      <CarForm onSubmitCar={addCar} buttonText="Add Car" />
     </>
   );
 
