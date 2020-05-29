eb805c13e387af30390835d56ff0bfb1add259a6
Eric Greene <eric@t4d.io>
2020-05-28 20:07:38 -0400

    add exercise 11

--- demo-app/src/components/CarEditRow.js
+++ demo-app/src/components/CarEditRow.js
@@ -19,11 +19,11 @@ export const CarEditRow = ({ car, onSaveCar, onCancelCar }) => {
   return (
     <tr>
       <td>{car.id}</td>
-      <td><input type="text" name="make" value={car.make} onChange={change} /></td>
-      <td><input type="text" name="model" value={car.model} onChange={change} /></td>
-      <td><input type="number" name="year" value={car.year} onChange={change} /></td>
-      <td><input type="text" name="color" value={car.color} onChange={change} /></td>
-      <td><input type="number" name="price" value={car.price} onChange={change} /></td>
+      <td><input type="text" name="make" value={carForm.make} onChange={change} /></td>
+      <td><input type="text" name="model" value={carForm.model} onChange={change} /></td>
+      <td><input type="number" name="year" value={carForm.year} onChange={change} /></td>
+      <td><input type="text" name="color" value={carForm.color} onChange={change} /></td>
+      <td><input type="number" name="price" value={carForm.price} onChange={change} /></td>
       <td>
         <button type="button" onClick={saveCar}>Save</button>
         <button type="button" onClick={onCancelCar}>Cancel</button>
